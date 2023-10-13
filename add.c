487b7c6dcf9b0f89adc3e44efa39e91d1bc90eca
sankalp <147638966+sankalp-2607@users.noreply.github.com>
2023-10-13 21:10:35 +0530

    Update add.c

---
+++ add.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+//PART 1
+typedef struct node
+{
+    int data;
+    struct node *next;
+}node;
+
+
+//PART 2
+
+void main(){
+    int n,i;
+    struct node *p,*head;
+    printf("Enter the no. of elements = ");
+    scanf("%d",&n);
+
+    head=(node*)mallloc(sizeof(node));
+    printf("Enter the element 1 = ");
+    scanf
+}
